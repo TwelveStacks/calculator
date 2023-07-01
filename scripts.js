0b441e816ed621b4d19ae25b8a6906f875b414ae
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-06-30 22:39:17 -0400

    Added decimal to show on display

--- scripts.js
+++ scripts.js
@@ -1,30 +1,37 @@
 const output = document.getElementById('outputDisplay');
 
 function updateDisplay() {
-    output.textContent = firstNumber + "" + operation + "" +secondNumber;
+    // Check if equation length is 0
+    if(equation.length == 0){
+        output.textContent = 0; // If equation length equal '' display 0
+    } else if(equation.charAt(0) == '.'){ // Check if equation = '.'
+        equation = '0.' // 
+        output.textContent = equation;
+    } else if(equation == '0'){ // If equation is 0 and a number is pressed
+        equation = ''; // Set equation to '' so that it will not have a 0 in front
+        output.textContent = 0; // Set display to show 0 while equation is blank
+    }
+    else {
+        output.textContent = equation; // Set output display to equation
+    }
 }
 
-// Numbers
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
-const button4 = document.getElementById('button4');
-const button5 = document.getElementById('button5');
-const button6 = document.getElementById('button6');
-const button7 = document.getElementById('button7');
-const button8 = document.getElementById('button8');
-const button9 = document.getElementById('button9');
-const button0 = document.getElementById('button0');
+let equation = '';
 
+// Register number button inputs
 let numberInput = document.querySelectorAll('.button')
 
 for(let i = 0; i < numberInput.length; i++){
     numberInput[i].addEventListener('click', ()=> {
         switch(operation){
             case '':
+                console.log("----------------Pressed " + numberInput[i].value + "--------------");
                 firstNumber += numberInput[i].value;
-                console.log(firstNumber);
-                output.textContent = firstNumber;
+                equation += firstNumber
+                console.log("First numnber = " + firstNumber)
+                firstNumber = '';
+                console.log("Equation = " + equation)
+                updateDisplay();
                 break;
             default:
                 secondNumber += numberInput[i].value;
@@ -33,6 +40,16 @@ for(let i = 0; i < numberInput.length; i++){
         }
     });
 }
+
+let backspace = document.getElementById('backspace');
+
+backspace.addEventListener('click', () => {
+    equation = equation.substring(0, equation.length-1)
+    firstNumber = '';
+    console.log("----------------Pressed Backspace--------------")
+    console.log("Equation = " + equation)
+    updateDisplay();
+});
 
 // Operators
 const divisionButton = document.getElementById('division');
@@ -45,10 +62,15 @@ const equalButton = document.getElementById('equal');
 const clearButton = document.getElementById('clear');
 
 clearButton.addEventListener('click', ()=> {
+    equation = '';
     firstNumber = '';
     secondNumber = '';
     operation = '';
-    output.textContent = 0;
+    updateDisplay();
+    console.log("----------------Pressed Clear--------------")
+    console.log("Equation = " + equation)
+    console.log("First number = " + equation)
+    
 });
 
 function add(a, b) {
